9bc3637bd99f8a5fc90495a076f7164b5e5af7b0
王沛晟 <115681033@qq.com>
2019-01-16 19:29:17 +0800

    add calculate button and fix bugs
    add calculate button and fix bugs

--- demo.py
+++ demo.py
@@ -3,12 +3,12 @@ from main import create_dict, shot_and_judge, card_sequence_to_link
 from web import open_url, web_init, web_close
 
 win = tkinter.Tk()
-# win.attributes("-alpha", 0.4)
+win.attributes("-alpha", 0.4)
 win.wm_attributes('-topmost', 1)
 win.title("Majsoul helper")
 win.geometry("700x200")
 
-title_label = tkinter.Label(win, text="")
+title_label = tkinter.Label(win, text="No data")
 title_label.pack()
 sim_label = tkinter.Label(win, text="")
 sim_label.pack()
@@ -21,33 +21,33 @@ hash_dict = create_dict()
 
 driver = web_init()
 
-def trickit():
+
+def calculate():
     names, sims = shot_and_judge(hash_dict)
     # names = ["3s", "4s"]
     link = card_sequence_to_link(names)
 
-    cards = ""
-    for name in names:
-        cards += name
-    title_label['text'] = cards
+    if len(names) % 3 == 2:
+        cards = ""
+        for name in names:
+            cards += name
+        title_label['text'] = cards
 
-    web_page = open_url(driver, link)
-    # print(web_page)
+        web_page = open_url(driver, link)
+        # print(web_page)
 
-    sim_label['text'] = str(sims)
+        sim_label['text'] = str(sims)
 
-    state = web_page.split("<div id=\"tehai\" align=\"center\">")[1].split("<br>")[0]
-    state_label['text'] = state
+        state = web_page.split("<div id=\"tehai\" align=\"center\">")[1].split("<br>")[0]
+        state_label['text'] = state
 
-    split_page = web_page.split(cards)
-    info = split_page[2].split("</textarea>")[0]
-    info_label['text'] = info
+        split_page = web_page.split(cards)
+        info = split_page[2].split("</textarea>")[0]
+        info_label['text'] = info
 
-    win.update()
-    win.after(1000, trickit)
+        win.update()
 
 
-win.after(1000, trickit)
-
+tkinter.Button(win, text="计算", command=calculate).pack()
 win.mainloop()
 web_close(driver)
